58cfd38b9399c9ff9dd181a34924fb7521a32d75
BunticMartin <martin.matija.buntic@fpmoz.sum.ba>
2022-05-12 17:01:27 +0200

    Drugi commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Staima</h1>
+    <input type="text" name="text" v-model="ticket.subject" /> <br> <br>
+    <input type="text" name="text" v-model="ticket.body" /> <br /> <br>
+    <button v-on:click="addUser()">Add New User</button>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      ticket: {
+        "subject": null,
+        "body": null
+      }
+    
+      
+    };
+  },
+  methods: {
+    async addUser() {
+      let result =await axios.post(process.env.VUE_APP_WHATEVERYOUWANT, this.ticket);
+      console.warn(result);
+    },
+  },
+};
 </script>
 
 <style>
